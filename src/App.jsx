ba9a6cd0ae6d6b2e87e714444742292cf9fb1588
Ahtisham-ul-hassan <164784447+hasan-345@users.noreply.github.com>
2024-04-27 13:11:36 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useRef, useState } from 'react'
 import './App.css'
 import WeatherApp from './WeatherApp'
 function App() {
-  const load = useRef();
-  window.onload = ()=>{
-    load.current.style.display = "none"
-  }
+  // const load = useRef();
+  // window.onload = ()=>{
+  //   load.current.style.display = "none"
+  // }
   return (
     <>
-    <div className="loading" ref={load} ><div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
+    <div className="loading"  ><div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
     <div className='big-cont'>
     <WeatherApp/>
     </div>
